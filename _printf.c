fbb5265db5ee374696369ff60da25596a5fa546b
selahadin meka <sulehibnumeka@gmail.com>
2023-01-23 15:31:39 +0300

    NEW

--- _printf.c
+++ _printf.c
@@ -1,1 +1,20 @@
-#
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+va_start(list, format);
