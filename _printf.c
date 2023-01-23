74908162ce67736525dd117f9607ece125387030
Daniel Yohannes <leinad.sennahoy@gmail.com>
2023-01-23 15:49:37 +0300

    while loop added

--- _printf.c
+++ _printf.c
@@ -17,3 +17,31 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 va_start(list, format);
+
+i = 0;
+while (format && format[i] != '\0')
+{
+    if (format[i] != '%')
+    {
+        buffer[buff_ind++] = format[i];
+        if (buff_ind == BUFF_SIZE)
+            print_buffer(buffer, &buff_ind);
+        printed_chars++;
+    }
+    else
+    {
+        print_buffer(buffer, &buff_ind);
+        flags = get_flags(format, &i);
+        width = get_width(format, &i, list);
+        precision = get_precision(format, &i, list);
+        size = get_size(format, &i);
+        ++i;
+        printed = handle_print(format, &i, list, buffer,
+            flags, width, precision, size);
+        if (printed == -1)
+            return (-1);
+        printed_chars += printed;
+    }
+    i++;
+}
+
