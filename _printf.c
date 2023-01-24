cbc2b6afcfb51a514743667c5daf9aee57da7401
Daniel Yohannes <leinad.sennahoy@gmail.com>
2023-01-24 19:29:22 +0300

    _printf.c betty coding errors fixed

--- _printf.c
+++ _printf.c
@@ -1,21 +1,10 @@
 #include "main.h"
-
- 
-
 void print_buffer(char buffer[], int *buff_ind);
-
- 
-
 /**
-
  * _printf - Printf function
-
  * @format: format.
-
  * Return: Printed chars.
-
  */
-
 int _printf(const char *format, ...)
 
 {
@@ -28,17 +17,11 @@ va_list list;
 
 char buffer[BUFF_SIZE];
 
- 
-
 if (format == NULL)
 
 return (-1);
 
- 
-
 va_start(list, format);
-
- 
 
 for (i = 0; format && format[i] != '\0'; i++)
 
@@ -90,30 +73,26 @@ printed_chars += printed;
 
 }
 
- 
+
 
 print_buffer(buffer, &buff_ind);
 
- 
+
 
 va_end(list);
 
- 
+
 
 return (printed_chars);
 
 }
 
- 
+
 
 /**
-
  * print_buffer - Prints the contents of the buffer if it exist
-
  * @buffer: Array of chars
-
  * @buff_ind: Index at which to add next char, represents the length.
-
  */
 
 void print_buffer(char buffer[], int *buff_ind)
@@ -124,7 +103,7 @@ if (*buff_ind > 0)
 
 write(1, &buffer[0], *buff_ind);
 
- 
+
 
 *buff_ind = 0;
 
